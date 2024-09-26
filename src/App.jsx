47c48d76c707438969eeb1204b12e86cafd4c50a
EnriqueMendozaGT <enriquemendoza0gt@gmail.com>
2024-09-25 22:57:05 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Hola Mundo</h1>
+    </div>
+  )
+}
+
+export default App
